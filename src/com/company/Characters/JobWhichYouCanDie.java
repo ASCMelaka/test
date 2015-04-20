a5ee27a2a85d4192f4c471fbc1cc6d03ccd519b6
navenprasad <navenprasad25@gmail.com>
2015-04-20 22:03:20 +0800

    Added Java Files
    Created Jobs class

---
+++ src/com/company/Characters/JobWhichYouCanDie.java
@@ -0,0 +1,21 @@
+package com.company.Characters;
+
+/**
+ * Created by navenprasad25 on 20/04/2015.
+ */
+public class JobWhichYouCanDie {
+
+    private int life;
+    protected boolean possesionOfGun;
+    protected boolean armor;
+
+    JobWhichYouCanDie(){
+        life = 100;
+        possesionOfGun = false;
+        armor = false;
+
+    }
+
+
+
+}
---
+++ src/com/company/Characters/Robber.java
@@ -0,0 +1,9 @@
+package com.company.Characters;
+
+/**
+ * Created by navenprasad25 on 20/04/2015.
+ */
+public class Robber {
+
+
+}

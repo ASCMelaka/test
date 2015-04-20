66ea293cddb5e74b37bfdf6ff2f2fc5f0d556607
wilson94 <wilson9_4tan@hotmail.com>
2015-04-20 22:26:45 +0800

    wilsoncodeformonday

---
+++ Wilson/NewClass.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class NewClass {
+
+    
+     int Lteaching(int Lteaching){
+         
+        return Lteaching;
+     }
+     int Lanswer(int Lanswer){
+         
+        return Lanswer;
+     }
+     int Squestion(int Squestion){
+         
+        return Squestion;
+     }
+      int Sanswer(int Sanswer){
+         
+        return Sanswer;
+     }
+}
